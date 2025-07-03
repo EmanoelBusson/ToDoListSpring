c97df6102ca563993aff2f341ff22ef31d031991
EmanoelBusson <emanoelbusson17@gmail.com>
2025-07-03 02:50:25 -0300

    Adicionando os arquivos com implementações e tratando problemas na hora de receber atualizações de task nula

---
+++ src/test/java/br/com/emanoelbusson/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emanoelbusson.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
